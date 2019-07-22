1333cb408cd39901b75e43a4bb4141570e28086c
Barbara Fick <bkantzos@yahoo.com>
2019-07-21 22:20:50 -0600

    adding Navigation component placeholder

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.module.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const layout = props => (
         <Aux>
-        <div>Toolbar, sideDrawer, Backdrop</div>
+        <Toolbar></Toolbar>
+        <div>sideDrawer, Backdrop</div>
         <main className={classes.Content}>
             {props.children}
         </main>
