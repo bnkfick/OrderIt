618cbd378396d2a4e0139794eec8fcd9d1276575
Barbara Fick <bkantzos@yahoo.com>
2019-07-22 18:48:21 -0600

    remove interceptors in withErrorHandler componentWillUnmount

--- src/App.js
+++ src/App.js
@@ -3,10 +3,20 @@ import Layout from './hoc/Layout/Layout';
 import DoodadBuilder from './containers/DoodadBuilder/DoodadBuilder';
 
 class App extends Component {
+  // state = {
+  //   show: true
+  // };
+
+  // componentDidMount() {
+  //   setTimeout(() => {
+  //     this.setState({show: false});
+  //   }, 5000);
+  // }
   render() {
     return (
       <div>
         <Layout>
+          {/*this.state.show ? <DoodadBuilder /> : null*/}
           <DoodadBuilder />
         </Layout>
       </div>
